0de4e9fc3bf9d84de375eee7f1487f6cb68aacc4
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-05-10 09:10:25 +0700

    change from ReservationDto to ReservationRequest

---
+++ src/main/java/com/workshop/pizza/controller/form/ReservationRequest.java
@@ -0,0 +1,36 @@
+package com.workshop.pizza.controller.form;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.workshop.pizza.dto.CustomerDto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationRequest {
+
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate reservationDate;
+	
+	@NotNull
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime reservationTime;
+	
+	@NotNull
+	@Min(value = 2, message = "Party size should be at least 2")
+	@Max(value = 6, message = "Party size should be at most 6")
+	private int partySize;
+	
+	@NotNull
+	private CustomerDto customer;
+}
