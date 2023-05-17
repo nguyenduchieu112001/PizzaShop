42cac3a2618d6c17e4ae63e179e988587662b139
nguyenduchieu112001 <nguyenduchieu112001@gmail.com>
2023-05-17 17:09:34 +0700

    change from ChangePasswordForm to ForgotPasswordForm

--- src/main/java/com/workshop/pizza/controller/form/ForgotPasswordForm.java
+++ src/main/java/com/workshop/pizza/controller/form/ForgotPasswordForm.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ChangePasswordForm {
+public class ForgotPasswordForm {
 
 	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%])(?=\\S+$).{8,}$", message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, no space sand the minimum length is 8")
 	private String password;
